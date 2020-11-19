6bf6e336ffbfbad57099150022746506941e0aa8
sunseok <dydrlaks@gmail.com>
2020-11-19 20:04:53 +0900

    feat: Create getTime() function to get current time from browser

--- clock.js
+++ clock.js
@@ -1,5 +1,16 @@
-function init() {
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
 
+function getTime() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    clockTitle.innerHTML = `${hours}:${minutes}:${seconds}`;
+}
+
+function init() {
+    getTime();
 }
 
 init();
