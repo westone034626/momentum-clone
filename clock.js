2d1d803a86bc60129a9b2c2c26e3dc234d956af1
westone034626 <dydrlaks@likelion.org>
2020-11-21 23:07:29 +0900

    feat: Edit 시간 표시기능(날짜까지 표시)

--- clock.js
+++ clock.js
@@ -1,14 +1,35 @@
 const clockContainer = document.querySelector(".js-clock");
-const clockTitle = clockContainer.querySelector("h1");
+const clockDate = document.querySelector(".js-clock-date")
+const clockTitle = document.querySelector(".js-clock-time");
+const dayOfWeeks = {
+    0: "일요일",
+    1: "월요일",
+    2: "화요일",
+    3: "수요일",
+    4: "목요일",
+    5: "금요일",
+    6: "토요일"
+}
+
+function getTodayDate(y, m, d, dOW) {
+    return `${y}년 ${m}월 ${d}일 ${dOW}`;
+}
+
+function getTodayTime(h, m, s) {
+    return `${h < 10? `0${h}`:h}:${m < 10? `0${m}`:m}:${s < 10? `0${s}`:s}`;
+}
 
 function getTime() {
     const date = new Date();
+    const years = date.getFullYear();
+    const months = date.getMonth();
+    const days = date.getDate();
+    const dayOfWeek = dayOfWeeks[date.getDay()];
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    clockTitle.innerHTML = `${hours < 10 ? `0${hours}` : hours}:${
-        minutes < 10 ? `0${minutes}` : minutes
-    }:${seconds < 10 ? `0${seconds}` : seconds}`;
+    clockDate.innerHTML = getTodayDate(years, months, days, dayOfWeek);
+    clockTitle.innerHTML = getTodayTime(hours, minutes, seconds);
 }
 
 function init() {
