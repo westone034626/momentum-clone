440bdb721bd1c1521e2450357e9d4ab3eb66d4e0
westone034626 <dydrlaks@likelion.org>
2020-12-05 17:37:07 +0900

    feat: Create pop-up effect after input name

--- src/gretting.js
+++ src/gretting.js
@@ -1,43 +1,50 @@
 const form = document.querySelector(".js-form"),
-    input = form.querySelector("input"),
-    greeting = document.querySelector(".js-greetings");
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings"),
+  clock = document.querySelector(".clock"),
+  footer = document.querySelector("footer"),
+  todoList = document.querySelector(".todoList");
 
 const USER_LS = "currentUser",
-    SHOWING_CN = "showing";
+  SHOWING_CN = "showing";
 
-function saveName(text){
-    localStorage.setItem(USER_LS, text);
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
 }
 
-function handleSubmit(event){
-    event.preventDefault();
-    const currentValue = input.value;
-    paintGreeting(currentValue);
-    saveName(currentValue);
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
 }
 
 function askForName() {
-    form.classList.add(SHOWING_CN);
-    form.addEventListener("submit", handleSubmit);
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
 }
 
 function paintGreeting(text) {
-    form.classList.remove(SHOWING_CN);
-    greeting.classList.add(SHOWING_CN);
-    greeting.innerHTML = `Hello ${text}`;
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  todoList.classList.add(SHOWING_CN);
+  clock.classList.add(SHOWING_CN);
+  footer.classList.add(SHOWING_CN);
+
+  greeting.innerHTML = `Hello ${text}`;
 }
 
 function loadName() {
-    const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser === null) {
-        askForName();
-    } else {
-        paintGreeting(currentUser);
-    }
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askForName();
+  } else {
+    paintGreeting(currentUser);
+  }
 }
 
 function init() {
-    loadName();
+  loadName();
 }
 
 init();
