fa665d8e24dec59ce58a9080d97390258c8a4d6f
westone034626 <dydrlaks@likelion.org>
2020-12-22 20:30:29 +0900

    refactor: Edit functions for distribution on clock.js

--- src/clock.js
+++ src/clock.js
@@ -1,17 +1,11 @@
 const clockContainer = document.querySelector(".js-clock");
 const clockDate = document.querySelector(".js-clock-date");
 const clockTitle = document.querySelector(".js-clock-time");
-const dayOfWeeks = {
-  0: "SUN",
-  1: "MON",
-  2: "TUE",
-  3: "WED",
-  4: "THU",
-  5: "FRI",
-  6: "SAT",
-};
 
-function getTodayDate(y, m, d) {
+function getTodayDate(date) {
+  const y = date.getFullYear();
+  const m = date.getMonth();
+  const d = date.getDate();
   return `
     ${y}. 
     ${m + 1 < 10 ? `0${m + 1}` : m + 1}.
@@ -19,28 +13,29 @@ function getTodayDate(y, m, d) {
   `;
 }
 
-function getTodayTime(h, m, s) {
+function getTodayTime(date) {
+  const h = date.getHours();
+  const m = date.getMinutes();
+  const s = date.getSeconds();
   return `${h < 10 ? `0${h}` : h}:${m < 10 ? `0${m}` : m}:${
     s < 10 ? `0${s}` : s
   }`;
 }
 
+function setClock(date) {
+  clockDate.innerHTML = getTodayDate(date);
+  clockTitle.innerHTML = getTodayTime(date);
+}
+
 function getTime() {
   const date = new Date();
-  const years = date.getFullYear();
-  const months = date.getMonth();
-  const days = date.getDate();
-  const dayOfWeek = dayOfWeeks[date.getDay()];
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
-  clockDate.innerHTML = getTodayDate(years, months, days);
-  clockTitle.innerHTML = getTodayTime(hours, minutes, seconds);
+  return date;
 }
 
 function init() {
-  getTime();
-  setInterval(getTime, 1000);
+  const time = getTime();
+  setClock(time);
 }
 
 init();
+setInterval(init, 1000);
